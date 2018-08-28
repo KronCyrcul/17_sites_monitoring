da8b1704892356b6c8c15d38471e633071a055f8
KronCyrcul <ibloko1504@gmail.com>
2018-08-28 18:53:34 +0300

    Renamed values, used requests instead of urllib module for checking server response, added exceptions in is_server_respond_with_200,get_domain_expiration_date, added check_expiration_date funtion

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlparse
-from urllib.request import urlopen
+import requests
 import datetime
 import whois
 import sys
@@ -8,37 +8,45 @@ import sys
 def load_urls4check(path):
     try:
         with open(path, "r") as sites_file:
-            sites = sites_file.read().split()
-        return sites
+            urls = sites_file.read().split()
+        return urls
     except FileNotFoundError:
         return None
 
 
 def is_server_respond_with_200(url):
-    server_response = urlopen(url).getcode()
-    if server_response == 200:
-        return True
-    else:
+    try:
+        server_response = requests.get(url)
+        return server_response.ok
+    except requests.exceptions.RequestException:
         return False
 
 
 def get_domain_expiration_date(domain_name):
-    domain_info = whois.whois(domain_name)
-    if type(domain_info["expiration_date"]) is list:
-        return domain_info["expiration_date"][0]
-    else:
-        return domain_info["expiration_date"]
+    try:
+        domain_info = whois.whois(domain_name)
+        if type(domain_info["expiration_date"]) is list:
+            return domain_info["expiration_date"][0]
+        else:
+            return domain_info["expiration_date"]
+    except whois.parser.PywhoisError:
+        return False
+
+
+def check_expiration_date(expiration_date, today, days_count):
+    try:
+        time_before_expiration = expiration_date - today
+        if time_before_expiration.days > days_count:
+            return "Yes, {} before expire".format(time_before_expiration.days)
+    except TypeError:
+        return False
 
 
 def print_site_health(url, server_response, time_before_expiration):
     print("Site URL: {}".format(url))
     print("Status code is 200: {}".format(server_response))
-    if time_before_expiration.days > 31:
-        print("Paid up front for a month: Yes,{} days before expire".format(
-            time_before_expiration.days))
-    else:
-        print("Paid up front for a month: No,expired {} days ago".format(
-            time_before_expiration.days*-1))
+    print("Paid up front for a month: {}".format(
+        time_before_expiration))
 
 
 if __name__ == "__main__":
@@ -46,13 +54,16 @@ if __name__ == "__main__":
         filepath = sys.argv[1]
     except IndexError:
         sys.exit("Enter path to file")
-    sites = load_urls4check(filepath)
-    if sites is None:
+    urls = load_urls4check(filepath)
+    if urls is None:
         sys.exit("File not found")
     today = datetime.datetime.today()
-    for site in sites:
-        server_response = is_server_respond_with_200(site)
-        domain_name = urlparse(site).hostname
+    days_count = 31
+    for url in urls:
+        server_response = is_server_respond_with_200(url)
+        domain_name = urlparse(url).hostname
         expiration_date = get_domain_expiration_date(domain_name)
-        time_before_expiration = expiration_date - today
-        print_site_health(site, server_response, time_before_expiration)
+        time_before_expiration = check_expiration_date(expiration_date,
+                                                       today,
+                                                       days_count)
+        print_site_health(url, server_response, time_before_expiration)
